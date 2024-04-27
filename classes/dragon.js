a99ac2de54ab32441519b98bb213478e8236bffc
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-27 00:57:45 -0500

    dragons almost solved

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,17 @@
-// Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...monsters) {
+    for (let monster of monsters) {
+      return monster.name;
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
